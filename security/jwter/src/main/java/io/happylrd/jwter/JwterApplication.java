09d4f0a7a237486ccb05182533c26c0001d219c1
happylrd <happylrd17@gmail.com>
2017-07-03 14:05:10 +0800

    First commit for jwter

---
+++ security/jwter/src/main/java/io/happylrd/jwter/JwterApplication.java
@@ -0,0 +1,12 @@
+package io.happylrd.jwter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwterApplication.class, args);
+	}
+}
