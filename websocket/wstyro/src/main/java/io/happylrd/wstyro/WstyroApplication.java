d13536cd256b7b17f72577d9582e7eac8871bab1
happylrd <happylrd17@gmail.com>
2017-07-11 16:48:25 +0800

    Add wstyro for learning websocket

---
+++ websocket/wstyro/src/main/java/io/happylrd/wstyro/WstyroApplication.java
@@ -0,0 +1,12 @@
+package io.happylrd.wstyro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WstyroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WstyroApplication.class, args);
+	}
+}
